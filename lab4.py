b2523f035a93fc207509b25bf1b64531f0b10660
Mangkanorsh <95523031+Mangkanorsh@users.noreply.github.com>
2024-03-29 14:57:45 +0800

    miss ko na sya

---
+++ lab4.py
@@ -0,0 +1,13 @@
+firstName = input("Enter First Name: ")
+midlleInitial = input("Enter Middle Initials: ")
+surname = input("Enter  Surname: ")
+gender = input("Enter Gender: ")
+course = input("Enter Course: ")
+studentNumber = input("Enter Student Number: ")
+
+print("\nHello ", firstName, midlleInitial, surname)
+print(gender," is your gender")
+print("You are ", gender)
+print("Student NUmber is: ", studentNumber)
+
+
