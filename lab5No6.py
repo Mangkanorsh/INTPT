9f61950f8818a6228f909409e4511d269272f400
Mangkanorsh <95523031+Mangkanorsh@users.noreply.github.com>
2024-03-29 23:21:22 +0800

    mahal ko pa

--- lab5No6.py
+++ lab5No6.py
@@ -1,35 +1,36 @@
 assignment = float(input("Assignment and Seatwork: "))/100 * 100
 
-quiz = float(input("Quiz: ")) 
-
-if quiz > 30:
-    print("score must not exceed 30")
+if assignment > 100:
+    print("Must not exceed 100")
 else:
-    quiz = quiz / 30 * 100
+    quiz = float(input("Quiz: ")) 
 
-
-    labExercise = float(input("Laboratory Exercises: ")) /100 * 100
-
-    exam = float(input("Exam: "))
-
-    if exam > 50:
-        print("score must not exceed 50")
+    if quiz > 30:
+        print("score must not exceed 30")
     else:
-        exam = exam / 50 * 100
-        grade = (assignment + quiz + labExercise + exam) /400 * 100
-        print("\nAssignment: ",assignment)
-        print("quiz: ",quiz)
-        print("Laboratory Exercises: ",labExercise)
-        print("Exams: ",exam)
+        quiz = quiz / 30 * 100
+        labExercise = float(input("Laboratory Exercises: ")) /100 * 100
 
-        print("\nYour grade is: ",grade)
-
-        if grade >= 75:
-            print("\nPassed")
-        elif grade > 100:
-            print("grade must not be more than 100")
+        if labExercise > 100:
+            print("Must not exceed 100")
         else:
-            print("\nFailed")
-    
+            exam = float(input("Exam: "))
 
-    
+            if exam > 50:
+                print("score must not exceed 50")
+            else:
+                exam = exam / 50 * 100
+                grade = (assignment + quiz + labExercise + exam) /400 * 100
+                print("\nAssignment: ",assignment)
+                print("quiz: ",quiz)
+                print("Laboratory Exercises: ",labExercise)
+                print("Exams: ",exam)
+
+                print("\nYour grade is: ",grade)
+
+                if grade >= 75:
+                    print("\nPassed")
+                elif grade > 100:
+                    print("grade must not be more than 100")
+                else:
+                    print("\nFailed")
